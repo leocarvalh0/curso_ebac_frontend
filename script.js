0028a7c98d5a3007a2c65f1f491e9c64784d6af4
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2023-11-24 16:03:16 -0300

    adicionando o exercicio do modulo 7

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form-deposito');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const campoA = document.getElementById('campoA');
+    const campoB = document.getElementById('campoB');
+
+    const messageSucess = document.querySelector('.message-sucess');
+    const messageError = document.querySelector('.message-error');
+
+    if (campoA.value < campoB.value) {  
+        messageError.style.display = 'none';
+        messageSucess.style.display = 'block'
+    } else {
+        messageSucess.style.display = 'none';
+        messageError.style.display = 'block';
+    }
+
+    campoA.value = '';
+    campoB.value = '';
+})
