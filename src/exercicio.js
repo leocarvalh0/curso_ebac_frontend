cbfcdf15d76352e7d6afffa7865f20c40d81fd3d
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2024-06-21 02:24:09 -0300

    adicionando o exercicio_ts

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplicar(2, 2));
+function dizOla(nome) {
+    return `Olá ${nome}, prazer em te conhecer!`;
+}
+console.log(dizOla('Leonardo'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+console.log(multiplicar(2, 2));
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}, prazer em te conhecer!`;
+}
+
+console.log(dizOla('Leonardo'));
